b3cc02a1f8d749281ad84a134b180357858304f4
tonyalonzoc <tonyalonzoc@gmail.com>
2023-08-21 13:17:47 -0600

    Carousel and Item component

---
+++ app/carousel.js
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { Carousel } from "@material-tailwind/react";
+
+export default function CustomCarousel({ images, activeImage }) {
+  return (
+    <Carousel
+      className="rounded-xl"
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
+        </div>
+      )}
+    >
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt="image 1"
+          className="h-full w-full object-cover"
+        />
+      ))}
+    </Carousel>
+    /*<img className="w-auto h-[26rem]" src={images[activeImage]}></img>*/
+  );
+}
---
+++ app/item.js
@@ -0,0 +1,78 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import CustomCarousel from "./carousel";
+
+function Item({ data }) {
+  const [activeImage, setCurrentImage] = useState(0);
+  let currentCarousel = 0;
+  const COLORS = Object.entries(data.colors);
+  const [activeColor, setCurrentColor] = useState(0);
+  const [photos, setCurrentPhotos] = useState(
+    data.colors["item-0"].photos.split(",")
+  );
+
+  const handleImageClick = (index) => {
+    setCurrentImage(index);
+  };
+
+  const handleDotClick = (index) => {
+    let photosArray = getPhotos(index);
+    setCurrentPhotos(photosArray);
+    if (photosArray.length < getPhotos(currentCarousel).length) {
+      setCurrentImage(0);
+    }
+    currentCarousel = index;
+    setCurrentColor(index);
+  };
+
+  function getPhotos(index) {
+    return data.colors["item-" + index].photos.split(",");
+  }
+
+  return (
+    <div className="w-[40rem]">
+      <div>
+        <CustomCarousel
+          images={photos}
+          activeImage={activeImage}
+        ></CustomCarousel>
+
+        <div className="indicator">
+          {/*photos.map((_, index) => (
+            <button
+              className={
+                (activeImage == index ? "opacity-100" : "opacity-30") +
+                " mx-[3px] box-content h-[3px] w-[30px] flex-initial cursor-pointer border-0 border-y-[10px] border-solid border-transparent bg-black bg-clip-padding p-0 -indent-[999px] transition-opacity duration-[600ms] ease-[cubic-bezier(0.25,0.1,0.25,1.0)] motion-reduce:transition-none"
+              }
+              key={index}
+              onClick={() => handleImageClick(index)}
+            ></button>
+            ))*/}
+        </div>
+      </div>
+
+      <div className="flex justify-between ">
+        <div className="transition-all font-bold text-xl mb-2 hover:tracking-wider">
+          {data.name}
+        </div>
+
+        <div className="indicator space-x-5 ">
+          {COLORS.map((color, index) => (
+            <button
+              style={{ backgroundColor: color[1].color_code }}
+              className={
+                (activeColor == index ? "opacity-100" : "opacity-50") +
+                " w-5 h-5 rounded-full outline-1 outline outline-offset-2 outline-black"
+              }
+              key={index}
+              onClick={() => handleDotClick(index)}
+            ></button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
