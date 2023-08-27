ec3dddbde0a92fbe095b6e6f6f0a049c45bd934b
tonyalonzoc <tonyalonzoc@gmail.com>
2023-08-26 22:37:27 -0600

    Add filter functionality

--- app/filter.js
+++ app/filter.js
@@ -4,44 +4,93 @@ import React, { useState, useEffect } from "react";
 import {
   Checkbox,
   Card,
+  Button,
   List,
   ListItem,
   ListItemPrefix,
   Typography,
 } from "@material-tailwind/react";
 
-export default function Filter() {
+export default function Filter({ onApplyFilterClick }) {
   const TYPES = [
-    "Todo",
-    "Envases",
-    "Bolsas",
-    "Velas",
-    "Papelería",
-    "Editorial",
+    { label: "Todo", value: "todo" },
+    { label: "Envases", value: "envase" },
+    { label: "Bolsas", value: "bolsa" },
+    { label: "Velas", value: "vela" },
+    { label: "Papelería", value: "papeleria" },
+    { label: "Editorial", value: "editorial" },
   ];
   const MATERIALS = [
-    "Fibras orgánicas",
-    "Vidrio",
-    "Madera",
-    "Orgánicos",
-    "Papel",
-    "Metal",
+    { label: "Fibras orgánicas", value: "fibra-organica" },
+    { label: "Vidrio", value: "vidrio" },
+    { label: "Madera", value: "madera" },
+    { label: "Orgánicos", value: "organico" },
+    { label: "Papel", value: "papel" },
+    { label: "Metal", value: "metal" },
   ];
+  const [materials, setMaterials] = useState([]);
+  const [types, setTypes] = useState([]);
+
+  function handleTypeCheckbox(event, type) {
+    if (event.target.checked) {
+      if (type == "todo") {
+        setTypes(["todo", "envase", "bolsa", "vela", "papeleria", "editorial"]);
+      } else {
+        setTypes([...types, type]);
+      }
+    } else {
+      if (type == "todo") {
+        setTypes([]);
+      } else {
+        const updatedTypes = [...types];
+        updatedTypes.splice(types.indexOf(type), 1);
+        setTypes(updatedTypes);
+      }
+    }
+  }
+
+  function handleMaterialCheckbox(event, material) {
+    if (event.target.checked) {
+      setMaterials([...materials, material]);
+    } else {
+      const updatedMaterials = [...materials];
+      updatedMaterials.splice(materials.indexOf(material), 1);
+      setMaterials(updatedMaterials);
+    }
+  }
+
+  function handleDeleteAllClick() {
+    setTypes([]);
+    setMaterials([]);
+  }
 
   return (
     <div>
       <p className="uppercase font-bold">Filtros y clasificación</p>
       <List>
         <p className="font-bold">Tipo de producto</p>
-        {TYPES.map((type) => (
-          <ListItem className="p-0">
+        {TYPES.map((type, index) => (
+          <ListItem className="p-0" key={index}>
             <label
-              htmlFor={"vertical-list-" + type}
+              htmlFor={"vertical-list-" + type.label}
               className="flex w-full cursor-pointer items-center px-3 py-2"
             >
               <ListItemPrefix className="mr-3">
                 <Checkbox
-                  id={"vertical-list-" + type}
+                  id={"vertical-list-" + type.label}
+                  /*
+                  onClick={() => {
+                    onTypeCheckboxClick(type.value);
+                  }}*/
+                  onChange={(event) => {
+                    handleTypeCheckbox(event, type.value);
+                  }}
+                  /*
+                  onClick={() => {
+                    addToCategoryFilter(type.value);
+                  }}
+                  */
+                  checked={types.includes(type.value)}
                   ripple={false}
                   className="hover:before:opacity-0"
                   containerProps={{
@@ -50,7 +99,7 @@ export default function Filter() {
                 />
               </ListItemPrefix>
               <Typography color="blue-gray" className="font-medium">
-                {type}
+                {type.label}
               </Typography>
             </label>
           </ListItem>
@@ -59,29 +108,60 @@ export default function Filter() {
 
       <List>
         <p className="font-bold">Material</p>
-        {MATERIALS.map((material) => (
-          <ListItem className="p-0">
+        {MATERIALS.map((material, index) => (
+          <ListItem className="p-0" key={index}>
             <label
-              htmlFor={"vertical-list-" + material}
+              htmlFor={"vertical-list-" + material.label}
               className="flex w-full cursor-pointer items-center px-3 py-2"
             >
               <ListItemPrefix className="mr-3">
                 <Checkbox
-                  id={"vertical-list-" + material}
+                  id={"vertical-list-" + material.label}
                   ripple={false}
                   className="hover:before:opacity-0"
                   containerProps={{
                     className: "p-0",
                   }}
+                  /*
+                  onClick={onMaterialCheckboxClick}
+                  */
+                  onChange={(event) => {
+                    handleMaterialCheckbox(event, material.value);
+                  }}
+                  /*
+                  onClick={() => {
+                    addToCategoryFilter(type.value);
+                  }}
+                  */
+                  checked={materials.includes(material.value)}
                 />
               </ListItemPrefix>
               <Typography color="blue-gray" className="font-medium">
-                {material}
+                {material.label}
               </Typography>
             </label>
           </ListItem>
         ))}
       </List>
+
+      <div className="flex flex-row gap-x-6 pt-6">
+        <Button
+          className="normal-case"
+          variant="outlined"
+          onClick={handleDeleteAllClick}
+        >
+          Borrar todo
+        </Button>
+        <Button
+          className="normal-case"
+          variant="outlined"
+          onClick={() => {
+            onApplyFilterClick(types, materials);
+          }}
+        >
+          Aplicar
+        </Button>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -9,29 +9,60 @@ import Item from "./item";
 import { Checkbox } from "@material-tailwind/react";
 
 export default function Home() {
-  const CATEGORIES = [];
   const [items, setItems] = useState([]);
+  const [itemList, setItemList] = useState([]);
+
   useEffect(() => {
     fetch("https://mktideas.com/wp-json/jet-cct/shop_item")
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setItems(data);
+        setItemList(
+          data.map((item) => {
+            return <Item key={item._ID} data={item}></Item>;
+          })
+        );
+        console.log(itemList);
       })
       .catch((err) => {
         console.log(err.message);
       });
   }, []);
 
-  const itemList = items.map((item) => {
-    return <Item key={item._ID} data={item}></Item>;
-  });
+  function handleApplyFilterClick(types, materials) {
+    setItemList(
+      items
+        .filter((item) => {
+          let filter = true;
+
+          if (types.length) {
+            filter = types.includes(item.type);
+          }
+
+          if (filter && materials.length) {
+            filter = materials.includes(...item.material);
+          }
+
+          return filter;
+        })
+        .map((item) => {
+          return <Item key={item._ID} data={item}></Item>;
+        })
+    );
+  }
+
+  /*
+  function handleMaterialCheckboxClick() {
+    console.log("Material");
+  }
+  */
 
   return (
     <div className="bg-white">
       <main className="w-11/12 grid grid-cols-[1fr_2fr] p-10">
         <div>
-          <Filter></Filter>
+          <Filter onApplyFilterClick={handleApplyFilterClick}></Filter>
         </div>
         <div className="grid grid-cols-2 gap-x-8 gap-y-16">{itemList}</div>
       </main>
